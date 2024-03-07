169a0c4efee9c54486cf87a11ce79faecf114c97
jetfuzz <1jsf23@gmail.com>
2024-03-07 14:21:43 -0600

    Create function to store users input

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 //Create function for computer to generate rock, paper or scissors randomly
 
-let choices = ["rock", "paper", "scissors"]
+let choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    let randomChoice = Math.floor(Math.random()*choices.length)
-    return randomChoice
+    let randomChoice = Math.floor(Math.random()*choices.length);
+    return randomChoice;
 }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
+
+//Create function for user input to choose rock, paper or scissors
+
+function getUserChoice() {
+    let userChoice = prompt("Choose rock, paper or scissors:");
+    return userChoice.toLowerCase();
+}
+
+console.log(getUserChoice());
